74f6172c70d138a0dadf44da7194409116c01a4f
rajkumar-sendhav <131992728+rajkumar-sendhav@users.noreply.github.com>
2024-01-25 11:32:12 +0530

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import CameraScreen from './src/CameraScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <CameraScreen />
